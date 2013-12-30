7a5b690aba0823e78e6299b4322d9d1d568cb053
NekOSane <kennethso@lycos.com>
2013-12-30 14:32:03 +0800

    CLIENT: shuffle the pieces before placing them on the board

--- js/app_page.js
+++ js/app_page.js
@@ -78,7 +78,7 @@
 
     var init = function(side, socket) {
         constructBoard(side);
-        constructPieces(BOARD.PIECES, side);
+        constructPieces(shuffle(BOARD.PIECES), side);
 
         $('#readyBtn').click(function() {
             var pieces = $('.piece.' + side);
@@ -152,6 +152,27 @@
 
     var _isWaiting = function() {
         return (__moveId__) ? true : false;
+    };
+
+    var shuffle = function (array) {
+        var currentIndex = array.length
+            , temporaryValue
+            , randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
     };
 
     $(document).ready(function() {
